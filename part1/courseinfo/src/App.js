b5c0a4bfd4d0b462b97e41b309f1e72fe9401481
Emil Kelhälä <emil.kelhala@gmail.com>
2023-05-26 18:36:44 +0300

    Completed courseinfo steps 1,2

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -6,17 +6,20 @@ const Header = (props) => {
   )
 }
 
+const Part = (props) => {
+  return(
+    <p>
+      {props.name} {props.exercises}
+    </p>
+  )
+}
+
 const Content = (props) => {
-  const parts = props.parts.map((part) => {
-    return(
-      <p key={part.name}>
-        {part.name} {part.exercises}
-      </p>
-    )
-  })
   return(
   <div>
-    {parts}
+    <Part name={props.part1} exercises={props.exercises1}/>
+    <Part name={props.part2} exercises={props.exercises2}/>
+    <Part name={props.part3} exercises={props.exercises3}/>
   </div>
   )
 }
@@ -41,9 +44,8 @@ const App = () => {
   return (
     <div>
       <Header course={course}/>
-      <Content parts={[{name: part1, exercises: exercises1},
-                      {name: part2, exercises: exercises2},
-                      {name: part3, exercises: exercises3}]}/>
+      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2}
+       part3={part3} exercises3={exercises3}/>
       <Total total={(exercises1+exercises2+exercises3)}/>
     </div>
   )
